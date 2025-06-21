82a5343ea4b5a8a675a8b3f8e4344e974928c62f
Mahendra <mahendra9889@gmail.com>
2025-06-21 11:01:04 +0530

    code

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+
 const app = express();
 const port = 5778;
 
@@ -10,8 +11,42 @@ app.use(express.json());
 let tasks = [];
 
 app.get("/tasks",(req, res) => {
-    res.json();
+    res.json(tasks);
 });
 
+app.post("/tasks", (req ,res) => {
+    const {task} = req.body;
+    if(task) {
+        tasks.push(task);
+        res.status(201).json({Message : "Tasks added.."});
+    } else {
+        res.status(400).json({ Error : "Task is require.."})
+    }
+
+}); 
+
+app.get("/", (req , res ) => {
+    
+   res.end(
+      "Hello World"
+   )
+    
+})
+
+
+app.delete("/tasks/:index" ,  (req ,res ) => {
+    const index = parseInt(req.params.index);
+    if(index >= 0 && index < tasks.length ) 
+    {
+        tasks.splice(index ,1)
+        res.status(201).json({ Message : "Tasks Deleted."});
+    } else {
+        res.status(400).json({ Message : "Task not found"});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server start at http://localhost:${port}`);
+});
 
 
